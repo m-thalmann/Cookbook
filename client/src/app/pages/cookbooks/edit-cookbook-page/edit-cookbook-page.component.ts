bc3e912121886ef5523a0b06e60c803a8bb3de04
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2023-06-02 20:49:08 +0200

    feat(edit-cookbook): added edit cookbook page with edit details

---
+++ client/src/app/pages/cookbooks/edit-cookbook-page/edit-cookbook-page.component.ts
@@ -0,0 +1,95 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
+import { BehaviorSubject, EMPTY, combineLatest, of, shareReplay, startWith, switchMap } from 'rxjs';
+import { ApiService } from 'src/app/core/api/api.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Logger as LoggerClass } from 'src/app/core/helpers/logger';
+import { handledErrorInterceptor } from 'src/app/core/rxjs/handled-error-interceptor';
+import { SnackbarService } from 'src/app/core/services/snackbar.service';
+import { CookbookHeaderComponent } from '../components/cookbook-header/cookbook-header.component';
+import { EditCookbookDetailsFormComponent } from './edit-cookbook-details-form/edit-cookbook-details-form.component';
+
+const Logger = new LoggerClass('Cookbooks');
+
+@Component({
+  selector: 'app-edit-cookbook-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TranslocoModule,
+    MatIconModule,
+    MatButtonModule,
+    CookbookHeaderComponent,
+    EditCookbookDetailsFormComponent,
+  ],
+  templateUrl: './edit-cookbook-page.component.html',
+  styleUrls: ['./edit-cookbook-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EditCookbookPageComponent {
+  private cookbookId$ = combineLatest([this.activatedRoute.params, this.auth.user$]).pipe(
+    switchMap(([params, _]) => {
+      if (params['id']) {
+        return of(params['id']);
+      }
+
+      Logger.error('EditCookbookPage: No id defined');
+
+      this.router.navigate(['/']);
+
+      return EMPTY;
+    })
+  );
+
+  updateCookbook$ = new EventEmitter<void>();
+
+  cookbook$ = combineLatest([this.cookbookId$, this.updateCookbook$.pipe(startWith(undefined))]).pipe(
+    switchMap(([cookbookId, _]) => this.api.cookbooks.get(cookbookId)),
+    switchMap((cookbook) => {
+      if (cookbook.body?.data.meta.is_admin) {
+        return of(cookbook);
+      }
+
+      Logger.error('EditCookbookPage: User cannot edit cookbook');
+      this.snackbar.warn('messages.errors.userCantEditCookbook', { translateMessage: true });
+
+      this.router.navigate(['/cookbooks', cookbook.body!.data.id]);
+
+      return EMPTY;
+    }),
+    handledErrorInterceptor(),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+
+  error$ = this.api.handleRequestError(this.cookbook$, (error) =>
+    Logger.error('Error while loading cookbook:', this.api.getErrorMessage(error), error)
+  );
+
+  detailsSaving$ = new BehaviorSubject(false);
+  usersSaving$ = new BehaviorSubject(false);
+  deleting$ = new BehaviorSubject(false);
+
+  constructor(
+    private api: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private auth: AuthService,
+    private snackbar: SnackbarService
+  ) {}
+
+  onDetailsSaving(saving: boolean) {
+    this.detailsSaving$.next(saving);
+  }
+
+  onUsersSaving(saving: boolean) {
+    this.usersSaving$.next(saving);
+  }
+
+  async deleteCookbook() {
+    // TODO: implement
+  }
+}
--- client/src/app/pages/cookbooks/routes.ts
+++ client/src/app/pages/cookbooks/routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/core/auth/auth.guard';
 import { CookbookDetailPageComponent } from './cookbook-detail-page/cookbook-detail-page.component';
 import { CookbookRecipesPageComponent } from './cookbook-recipes-page/cookbook-recipes-page.component';
 import { CookbooksPageComponent } from './cookbooks-page/cookbooks-page.component';
+import { EditCookbookPageComponent } from './edit-cookbook-page/edit-cookbook-page.component';
 
 export default [
   { path: '', component: CookbooksPageComponent, data: { showAddButton: true } },
@@ -9,6 +11,7 @@ export default [
     path: ':id',
     children: [
       { path: '', component: CookbookDetailPageComponent },
+      { path: 'edit', component: EditCookbookPageComponent, canActivate: [AuthGuard], runGuardsAndResolvers: 'always' },
       { path: 'recipes', component: CookbookRecipesPageComponent },
     ],
   },
