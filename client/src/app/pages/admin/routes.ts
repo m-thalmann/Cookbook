7b6156356222b8a6fe68b39827bafe8b9d196455
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2023-06-19 19:12:55 +0200

    feat(admin): added dashboard view

--- client/src/app/pages/admin/routes.ts
+++ client/src/app/pages/admin/routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardPageComponent } from './admin-dashboard-page/admin-dashboard-page.component';
 import { AdminPageLayoutComponent } from './admin-page-layout/admin-page-layout.component';
 import { AdminRecipesPageComponent } from './admin-recipes-page/admin-recipes-page.component';
 import { AdminUsersPageComponent } from './admin-users-page/admin-users-page.component';
 
 export default [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   {
     path: '',
     component: AdminPageLayoutComponent,
     children: [
+      {
+        path: 'dashboard',
+        component: AdminDashboardPageComponent,
+        data: { title: 'pages.admin.area' },
+      },
       {
         path: 'users',
         component: AdminUsersPageComponent,
