9a6601232e8aaa34e27f18215c667f6a4d0105dc
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2023-07-02 11:59:50 +0200

    test(prune-images): improved tests for prune images command

--- api/tests/Feature/Commands/PruneRecipeImagesTest.php
+++ api/tests/Feature/Commands/PruneRecipeImagesTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Commands;
 
 use App\Models\Recipe;
 use App\Models\RecipeImage;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery\MockInterface;
@@ -14,19 +16,40 @@ class PruneRecipeImagesTest extends TestCase {
 
     public function testItDeletesUnusedImages() {
         /**
+         * @var FilesystemAdapter
+         */
+        $storageFake = Storage::fake('public');
+
+        $unusedPath = RecipeImage::IMAGE_DIRECTORY . '/1.jpg';
+        $usedPath = RecipeImage::IMAGE_DIRECTORY . '/2.jpg';
+
+        $recipe = Recipe::factory()->create();
+        $image = $recipe->images()->make();
+        $image->image_path = $usedPath;
+        $image->save();
+
+        $storageFake->put($unusedPath, 'test');
+        $storageFake->put($usedPath, 'test');
+
+        $statusCode = $this->artisan('recipeImages:prune')->run();
+        $this->assertEquals(0, $statusCode);
+
+        $storageFake->assertMissing($unusedPath);
+        $storageFake->assertExists($usedPath);
+    }
+
+    public function testItDoesNothingIfTheImagesTableDoesNotExist() {
+        /**
          * @var RecipeImage|MockInterface
          */
         $recipeImageMock = Mockery::mock(RecipeImage::class)->makePartial();
         app()->instance(RecipeImage::class, $recipeImageMock);
 
-        $recipeImageMock->shouldReceive('getTable')->andReturn('recipe_images');
-
-        $amountDeleted = 2;
-
         $recipeImageMock
-            ->shouldReceive('pruneImages')
-            ->once()
-            ->andReturn($amountDeleted);
+            ->shouldReceive('getTable')
+            ->andReturn('non-existing-table');
+
+        $recipeImageMock->shouldNotReceive('pruneImages');
 
         $statusCode = $this->artisan('recipeImages:prune')->run();
         $this->assertEquals(0, $statusCode);
