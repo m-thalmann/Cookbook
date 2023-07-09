4af814641835156f793108c7f80ad95979c43a58
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2023-07-09 13:05:16 +0200

    feat(about): show link to api documentatio in about dialog

--- client/src/app/pages/settings/components/about-dialog/about-dialog.component.ts
+++ client/src/app/pages/settings/components/about-dialog/about-dialog.component.ts
@@ -2,15 +2,17 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { TranslocoModule } from '@ngneat/transloco';
 import { map } from 'rxjs';
+import { ConfigService } from 'src/app/core/services/config.service';
 import { ThemeService } from 'src/app/core/services/theme.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-about-dialog',
   standalone: true,
-  imports: [CommonModule, TranslocoModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, TranslocoModule, MatIconModule, MatDialogModule, MatButtonModule],
   templateUrl: './about-dialog.component.html',
   styleUrls: ['./about-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,9 +20,13 @@ import { environment } from 'src/environments/environment';
 export class AboutDialogComponent {
   isDarkTheme$ = this.theme.theme$.pipe(map((theme) => theme.scheme === 'dark'));
 
-  constructor(private theme: ThemeService) {}
+  constructor(private theme: ThemeService, private config: ConfigService) {}
 
   get version() {
     return environment.version;
+  }
+
+  get apiDocsLink() {
+    return this.config.get('apiUrl') + '/docs';
   }
 }
